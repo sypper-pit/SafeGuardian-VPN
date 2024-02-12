bcfded7b60cd4718bd6f49d23db82189d9d9bc97
sypper-pit <uuhn.info@gmail.com>
2024-02-12 22:18:15 +0300

    Add files via upload

---
+++ bash_local_lxc_v2.12/lxd_conf.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+
+apt update && apt upgrade -y && wait
+apt install lxc -y && wait
+apt install lxd -y && wait
+
+sleep 10 && wait
+
+cat <<EOF | lxd init --preseed
+config: {}
+networks:
+- config:
+    ipv4.address: auto
+    ipv4.nat: true
+    ipv6.address: none
+  description: ""
+  name: wan0
+  type: ""
+  project: default
+- config:
+    ipv4.address: 10.0.4.1/24
+    ipv4.nat: false
+    ipv4.dhcp: true
+    ipv6.address: none
+  description: ""
+  name: lan0
+  type: ""
+  project: default
+storage_pools:
+- config: {}
+  description: ""
+  name: default
+  driver: dir
+profiles:
+- config: {}
+  description: ""
+  devices:
+    eth0:
+      name: eth0
+      network: wan0
+      type: nic
+    eth1:
+      name: eth1
+      network: lan0
+      type: nic
+    root:
+      path: /
+      pool: default
+      type: disk
+  name: default
+projects: []
+cluster: null
+EOF
